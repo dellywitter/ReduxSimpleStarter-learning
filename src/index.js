12ee708607fd1eb6315da995af2bce37f19ad228
Delly Witter <delly.witter@gmail.com>
2018-05-14 15:50:00 +0300

    section 2

--- src/index.js
+++ src/index.js
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import YTSearch from 'youtube-api-search';
+import VideoDetail from './components/video_detail';
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY = 'AIzaSyCZ68sgglT0JcvuTIVqMGvXxEQCuX3Dazs';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+
+class App extends Component {
+
+    constructor (props){
+        super(props);
+
+        this.state={videos: []};
+
+        YTSearch({key:API_KEY, term:'surfboards'}, (videos) => {
+            this.setState({videos});
+        });
+
+    }
+    render(){
+        return (
+            <div>
+                <SearchBar />
+                <VideoDetail video={this.state.videos[0]}/>
+                <VideoList videos={this.state.videos} />
+            </div>
+        );
+    }
+
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
